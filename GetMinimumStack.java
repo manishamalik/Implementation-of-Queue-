d73bc8eedf7414b3d1a95fef1951f822319007da
Manisha Malik <manishamalik11@gmail.com>
2018-10-11 15:37:03 -0700

    ADD get minimum in O(1) time using stack

---
+++ GetMinimumStack.java
@@ -0,0 +1,35 @@
+package revisionQueue;
+
+import java.util.Stack;
+
+public class GetMinimumStack {
+    Stack<Integer> mainStack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+
+    public void push(int data){
+        mainStack.push(data);
+        if(minStack.empty() || minStack.peek()>data){
+            minStack.push(data);
+        }
+    }
+    public int pop(){
+        if(mainStack.empty()){
+            return Integer.MIN_VALUE;
+        }
+        int data= mainStack.pop();
+        if(minStack.peek()==data){
+            minStack.pop();
+        }
+        return data;
+    }
+    public int getMin(){
+        return minStack.peek();
+    }
+    public int top(){
+        return mainStack.peek();
+    }
+    public boolean isEmpty(){
+        return mainStack.empty();
+    }
+
+}
